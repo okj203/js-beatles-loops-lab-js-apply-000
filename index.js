9a2b44ba59b2c7fff36dc32b02ea6b8c5785a8f3
Ock Kyung Jung <okj203@gmail.com>
2020-04-16 19:18:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// add solution here
+let theBeatlesPlay = (musicians, instruments) => {
+  let array = [];
+  for (let i = 0; i < musicians.length; i++) {
+    let person = musicians[i];
+    array.push(person + ' plays' + ' ' + instruments[i]);
+  }
+  
+  return array;
+}
+
+
+let johnLennonFacts = facts => {
+  let array = [];
+  let i = 0
+ 
+  while (i < facts.length) {
+    let str = facts[i];
+    array.push(str + `!!!`);
+    
+  } i++;
+  
+  return array;
+}
